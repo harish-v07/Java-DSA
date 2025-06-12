aeccfe3203d3f9a879f9c347354417d1f6bc9af8
Harish V <vharish7100@gmail.com>
2025-06-12 14:20:35 +0530

    Update Median.java

--- Array/Median.java
+++ Array/Median.java
@@ -1,3 +1,32 @@
+/* You are given a r x c matrix where each row is sorted in ascending order. Your task is to find the median of all the elements in the matrix. Since the rows are sorted, but the overall matrix is not, the median is not necessarily at the center of the matrix. The median is defined as the middle value when all elements are sorted. If the total number of elements is odd, it's the middle one; if even, return the lower middle one. Instead of flattening and sorting the entire matrix, you can use a binary search between the minimum and maximum values and count how many elements are ≤ mid to find the correct median efficiently in O(32 * r * log c) time.
+
+Input Format:
+First line: Two integers r and c — rows and columns of the matrix
+Next r lines: Each line contains c space-separated integers (row-wise sorted)
+
+Output Format:
+Print a single integer — the median value of the matrix
+
+
+Constraints:
+1 ≤ r, c ≤ 500
+1 ≤ matrix[i][j] ≤ 10⁹
+Each row is sorted in ascending order
+Total number of elements (r × c) is always odd
+
+
+Example:
+Sample Input:
+3 3
+1 3 5
+2 6 9
+3 6 9
+
+Sample Output:
+5
+*/
+
+
 import java.util.*;
 public class Main {
     public static int countSmallerThanOrEqual(int row[], int target){
