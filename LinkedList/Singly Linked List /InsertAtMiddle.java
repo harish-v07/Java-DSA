5a48614fd805179580813d507f387fe03c7cc568
Harish V <vharish7100@gmail.com>
2025-06-13 13:28:12 +0530

    Create Insert At Middle

--- LinkedList/Singly Linked List /InsertAtMiddle.java
+++ LinkedList/Singly Linked List /InsertAtMiddle.java
@@ -10,62 +10,65 @@ Sample output:
 
 
 import java.util.*;
-class Node{
-  int data;
-  Node next;
-  Node(int val){
-    this.data = val;
-    this.next = null;
-  }
+public class Main {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        MyLinkedList l1 = new MyLinkedList();
+        int n = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            int num = s.nextInt();
+            l1.create(num);
+        }
+        int n1 = s.nextInt();
+        l1.insertAtMiddle(n1, n);
+        l1.display();
+    }
 }
-class LinkedList{
-    Node head = null, current = null;
-    void create(int n){
-      Node newnode = new Node(n);
-      if(head == null){
-        head = newnode;
-        current = newnode;
-      }
-      else{
-        current.next = newnode;
-        current = newnode;
-      }
+class Node {
+    int data;
+    Node next;
+    Node(int val) {
+        this.data = val;
+        this.next = null;
     }
-    void insertAtMiddle(int val, int n){
-      Node newnode = new Node(val);
-      if(head == null){
-        head = newnode;
-        current = newnode;
-      }
-      int mid = n/2;
-      Node current = head;
-      int i=0;
-      while(i<mid-1){
-        i++;
-        current = current.next;
-      }
-      newnode.next = current.next;
-      current.next = newnode;
-    }
-    void display(){
-      Node current = head;
-      while(current!=null){
-        System.out.print(current.data+" ");
-        current = current.next;
-      }
-    }
-public class Main{
-  public static void main(String args[]){
-    Scanner s = new Scanner(System.in);
-    LinkedList l1 = new LinkedList();
-    int n = s.nextInt();
-    for(int i=0; i<n; i++){
-      int num = s.nextInt();
-      l1.create(num);
-    }
-    int n1 = s.nextInt();
-    l1.insertAtMiddle(n1,n);
-    l1.display();
-  }
 }
+class MyLinkedList {
+    Node head = null;
+    Node tail = null;
+    void create(int n) {
+        Node newnode = new Node(n);
+        if (head == null) {
+            head = newnode;
+            tail = newnode;
+        } else {
+            tail.next = newnode;
+            tail = newnode;
+        }
+    }
+    void insertAtMiddle(int val, int n) {
+        Node newnode = new Node(val);
+        if (head == null) {
+            head = newnode;
+            return;
+        }
+        int mid = n / 2;
+        Node temp = head;
+        if (n == 1) {
+            newnode.next = temp.next;
+            temp.next = newnode;
+            return;
+        }
+        for (int i = 0; i < mid - 1; i++) {
+            temp = temp.next;
+        }
+        newnode.next = temp.next;
+        temp.next = newnode;
+    }
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
 }
