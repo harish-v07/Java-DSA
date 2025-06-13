c4cf5ac35758362eccff4501c8e59c3e732bb63e
Harish V <vharish7100@gmail.com>
2025-06-13 14:52:05 +0530

    Create Delete At Middle

--- LinkedList/Singly Linked List /DeleteAtMiddle.java
+++ LinkedList/Singly Linked List /DeleteAtMiddle.java
@@ -1,3 +1,34 @@
+/*Example:
+Sample Input:
+5
+10 20 30 40 50
+Sample Output:
+10 20 40 50
+
+
+
+Public Test Cases:
+#	INPUT	EXPECTED OUTPUT
+
+3
+1 2 3
+1 3
+
+4
+11 22 33 44
+11 33 44 
+	
+1
+99
+List is empty
+
+2
+7 14
+14
+*/
+
+
+
 import java.util.*;
 public class Main {
     public static void main(String args[]) {
@@ -34,7 +65,10 @@ class MyLinkedList {
         }
     }
     void deleteAtMiddle(int n) {
-        if(head==null)
+        if(n==2){
+          head = head.next; //Delete at beginning for 2 elements
+        }
+        else if(head==null)
           System.out.print("List is empty");
         else{
           if(head.next == null){
@@ -42,7 +76,7 @@ class MyLinkedList {
             head = head.next; 
             return;
           }
-          int mid = (n%2==0)? (n/2-1):(n/2);
+          int mid = (n%2==0)?(n/2-1):(n/2);
           Node temp = head;
           for (int i = 0; i < mid - 1; i++) {
             temp = temp.next;
