1690e8510ceb76a40b7ccefe75f6e2947a11bd05
Harish V <vharish7100@gmail.com>
2025-06-13 14:37:06 +0530

    Create Delete At End

---
+++ LinkedList/Singly Linked List /DeleteAtEnd.java
@@ -0,0 +1,67 @@
+/*
+Example:
+Sample Input:
+5
+10 20 30 40 50
+
+Sample output:
+10 20 30 40
+*/
+
+
+
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    Scanner s = new Scanner(System.in);
+    LinkedList l1 = new LinkedList();
+    int n = s.nextInt();
+    for(int i=0; i<n; i++){
+      int num = s.nextInt();
+      l1.create(num);
+    }
+    l1.deleteAtEnd();
+    l1.display();
+  }
+}
+class Node{
+  int data;
+  Node next;
+  Node(int val){
+    this.data = val;
+    this.next = null;
+  }
+}
+class LinkedList{
+    Node head = null, current = null;
+    void create(int n){
+      Node newnode = new Node(n);
+      if(head == null){
+        head = newnode;
+        current = newnode;
+      }
+      else{
+        current.next = newnode;
+        current = newnode;
+      }
+    }
+    void deleteAtEnd(){
+      if(head==null || head.next==null){
+        head=null;
+        System.out.print("List is empty");
+        return;
+      }
+      Node temp = head;
+      while (temp.next.next != null) {
+        temp = temp.next;
+      }
+    temp.next = null;
+    }
+    void display(){
+      Node current = head;
+      while(current!=null){
+        System.out.print(current.data+" ");
+        current = current.next;
+      }
+    }
+}
