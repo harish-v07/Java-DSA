de9e3dca3fd8461843bdd6e92abe5c27cb484224
Harish V <vharish7100@gmail.com>
2025-07-19 10:24:13 +0530

    Update Dutch National Flag Algorithm

--- Array/DutchNationalFlagAlgorithm.java
+++ Array/DutchNationalFlagAlgorithm.java
@@ -1,3 +1,16 @@
+/*
+Sort Colors
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+*/
+
+
 import java.util.*;
 public class Main{
   public static void main(String args[]){
