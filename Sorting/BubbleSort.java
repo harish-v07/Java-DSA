431682bfcec495c22a35f4af938603b7699778c9
Harish V <vharish7100@gmail.com>
2025-08-04 20:04:39 +0530

    Update Bubble Sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,24 +1,29 @@
 import java.util.Scanner;
 public class Main{
   public static void bubbleSort(int arr[], int n){
+    // boolean swapped;
     for(int i=0; i<n-1; i++){
+      swapped = false;
       for(int j=0; j<n-i-1; j++){
         if(arr[j] >arr[j+1]){
           int temp = arr[j];
           arr[j] = arr[j+1];
           arr[j+1] = temp;
+	  //swapped = true;
         }
       }
     }
+ //    if(!swapped)
+	// break;
   }
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for(int i=0;i<n;i++)
-		    arr[i] = sc.nextInt();
-		bubbleSort(arr,n);
-		for(int i=0;i<n;i++)
-		    System.out.print(arr[i]+" ");
-	}
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    for(int i=0;i<n;i++)
+      arr[i] = sc.nextInt();
+    bubbleSort(arr,n);
+    for(int i=0;i<n;i++)
+      System.out.print(arr[i]+" ");
+  }
 }
